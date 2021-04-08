325f56d4268b09c2f6b002ab6c82a40dec278ebb
Joe Bigelow <joebigelow@protonmail.com>
2021-04-07 22:01:57 -0500

    Refactor front end to abide by AirBnB eslint rules

--- client/App/Components/Results.jsx
+++ client/App/Components/Results.jsx
@@ -1,39 +1,39 @@
 import React from 'react';
 
 class Results extends React.Component {
-  constructor(props){
-      super(props)
-      this.state = {
-        showResult : '',
-        isShowing: false,
-        buttonText: 'Show Results'
-      }
-      this.buttonFunc = this.buttonFunc.bind(this)      
+  constructor(props) {
+    super(props);
+    this.state = {
+      showResult: '',
+      isShowing: false,
+      buttonText: 'Show Results',
+    };
+    this.buttonFunc = this.buttonFunc.bind(this);
   }
 
-  buttonFunc () {
-    if (this.state.isShowing === false) {  
-        const tempState = JSON.parse(JSON.stringify(this.state));
-        tempState.showResult = this.props.jsonRender;
-        tempState.isShowing = true;
-        tempState.buttonText = 'Hide Results';
-        this.setState(tempState);
+  buttonFunc() {
+    if (this.state.isShowing === false) {
+      const tempState = JSON.parse(JSON.stringify(this.state));
+      tempState.showResult = this.props.jsonRender;
+      tempState.isShowing = true;
+      tempState.buttonText = 'Hide Results';
+      this.setState(tempState);
     } else {
-        const tempState = JSON.parse(JSON.stringify(this.state));
-        tempState.showResult = '';
-        tempState.isShowing = false;        
-        tempState.buttonText = 'Show Results';
-        this.setState(tempState);
+      const tempState = JSON.parse(JSON.stringify(this.state));
+      tempState.showResult = '';
+      tempState.isShowing = false;
+      tempState.buttonText = 'Show Results';
+      this.setState(tempState);
     }
   }
-  
-  render(){
-    return(
-      <div className = 'individualResults'>
+
+  render() {
+    return (
+      <div className="individualResults">
         <button onClick={this.buttonFunc}>{this.state.buttonText}</button>
         <pre className="custom-json-pretty">{this.state.showResult}</pre>
       </div>
-    )
+    );
   }
 }
 
--- client/App/Components/SignupButton.jsx
+++ client/App/Components/SignupButton.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 // const [ details, setDetails ] = useState({});
 
-
-
 const SignupButton = () => {
-    function handleClick(e) {
-        e.preventDefault();
-    }
-    return(
-       <button className="signup" onClick={handleClick}>
-       <Link to='/signup'>
-         <i className="fas fa-user-plus"></i>Sign Up
-         </Link>
-       </button>
-       
-     
-    )
-}
+  function handleClick(e) {
+    e.preventDefault();
+  }
+  return (
+    <button type="button" className="signup" onClick={handleClick}>
+      <Link to="/signup">
+        <i className="fas fa-user-plus" />
+        Sign Up
+      </Link>
+    </button>
 
-export default SignupButton
+  );
+};
+
+export default SignupButton;
